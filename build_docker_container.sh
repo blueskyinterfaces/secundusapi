37c9c6d364cab2d37ee3649f3b086501bd4801be
blueskyinterfaces <nick@blueskyinterfaces.com>
2020-12-04 11:36:19 -0500

    repo migration complete

--- build_docker_container.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-(cd ~/go/src/github.com/blueskyinterfaces/secundusapiapiapi/assets/app && yarn && yarn lint --fix && yarn build)
-
-(cd ~/go/src/github.com/blueskyinterfaces/secundusapiapiapi && docker build -t secundusapi .)
